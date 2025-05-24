79e43b9b819ac24910db9f42043a23e804ce3c0a
SilentKiller725 <san.sap1011@gmail.com>
2025-05-24 13:23:36 +0545

    Delete basic projects directory

--- basic projects/extract.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url="https://en.wikipedia.org/wiki/List_of_European_Cup_and_UEFA_Champions_League_finals"
-
-res=requests.get(url)
-
-print(res.content)
